abf628c9d7ad0ba2f9295d7449510076db43c0a6
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-13 16:26:39 -0600

    "Commit to allow merge of updated master"

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(express.json());
 app.set('port', process.env.PORT || 3001);
 app.locals.title="Rancid Tomatillos api"
 
+app.get('/api/v1/rancid-tomatillos', (request, response) => {
+  response.send("Greetings from the depth of your machines!")
+});
+
+// app.get('/api/v1/rancid-tomatillos/favorites', () => {
+//   response.send
+// });
+
+app.post('api/vi/rancid-tomatillos/favorites', (request, response) => {
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}`)
 });
